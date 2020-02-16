973d37f6f6511c02b054eb0cb036e40332d95d6f
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-16 21:58:19 +0100

    implement noop transform

---
+++ test_transform.py
@@ -0,0 +1,13 @@
+import pytest
+
+from grid import Grid
+from transform import *
+
+
+@pytest.fixture
+def example_grid():
+    return Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+
+def test_noop(example_grid):
+    assert noop(example_grid) == example_grid
---
+++ transform.py
@@ -0,0 +1,2 @@
+def noop(grid):
+    return grid
