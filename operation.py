ae7a80ef951f38f08f8ead37dca9ecc9e10fd651
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-16 23:25:50 +0100

    implement elementwise logical operations

--- operation.py
+++ operation.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from grid import Grid
 
 
@@ -5,6 +7,73 @@ def noop(grid):
     return grid
 
 
-def extract_rect(grid, origin, shape):
-    rect = grid[origin[0] : shape[0] - origin[0], origin[1] : shape[1] - origin[1]]
-    return Grid(rect)
+def extract_rectangle(grid, origin, shape):
+    return grid[origin[0] : shape[0] - origin[0], origin[1] : shape[1] - origin[1]]
+
+
+def _check_shape_equals(a, b):
+    if a.shape != b.shape:
+        raise ValueError(
+            "arguments need to have the same shape, "
+            "but instead are a.shape={} and b.shape={}".format(a.shape, b.shape)
+        )
+    return a, b
+
+
+@np.vectorize
+def _elementwise_eand(a, b):
+    if a == 0:
+        return 0
+    elif b == 0:
+        return 0
+    elif a == b:
+        return a
+    else:
+        raise ValueError(
+            "elements need to be equal or at least one of them needs to be 0, "
+            "but instead are a[...]={} and b[...]={}".format(a, b)
+        )
+
+
+def elementwise_eand(a, b):
+    _check_shape_equals(a, b)
+    return Grid(_elementwise_eand(a.state, b.state))
+
+
+def _eor(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    elif a == b:
+        return a
+    else:
+        raise ValueError(
+            "elements need to be equal or at least one of them needs to be 0, "
+            "but instead are a[...]={} and b[...]={}".format(a, b)
+        )
+
+
+_elementwise_eor = np.vectorize(_eor)
+
+
+def elementwise_eor(a, b):
+    _check_shape_equals(a, b)
+    return Grid(_elementwise_eor(a.state, b.state))
+
+
+def _xor(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return 0
+
+
+_elementwise_xor = np.vectorize(_xor)
+
+
+def elementwise_xor(a, b):
+    _check_shape_equals(a, b)
+    return Grid(_elementwise_xor(a.state, b.state))
--- test_operation.py
+++ test_operation.py
@@ -13,6 +13,51 @@ def test_noop(example_grid):
     assert noop(example_grid) == example_grid
 
 
-def test_extract_rect(example_grid):
+def test_extract_rectangle(example_grid):
     extracted_grid = Grid([[1, 2], [4, 5], [7, 8]])
-    assert extract_rect(example_grid, origin=(0, 0), shape=(3, 2)) == extracted_grid
+    assert extract_rectangle(example_grid, origin=(0, 0), shape=(3, 2)) == extracted_grid
+
+
+def test_elementwise_operation__different_shapes():
+    a = Grid([[1, 2]])
+    b = Grid([[1, 2], [3, 4]])
+
+    with pytest.raises(ValueError):
+        elementwise_eand(a, b)
+
+
+def test_elementwise_eand():
+    a = Grid([[0, 0], [3, 4]])
+    b = Grid([[0, 2], [0, 4]])
+
+    assert elementwise_eand(a, b) == Grid([[0, 0], [0, 4]])
+
+
+def test_elementwise_eand__inequal_nonzero_elements():
+    a = Grid([[0, 0], [3, 4]])
+    b = Grid([[0, 2], [0, 5]])
+
+    with pytest.raises(Exception):
+        elementwise_eand(a, b)
+
+
+def test_elementwise_eor():
+    a = Grid([[0, 0], [3, 4]])
+    b = Grid([[0, 2], [0, 4]])
+
+    assert elementwise_eor(a, b) == Grid([[0, 2], [3, 4]])
+
+
+def test_elementwise_eor__inequal_nonzero_elements():
+    a = Grid([[0, 0], [3, 4]])
+    b = Grid([[0, 2], [0, 5]])
+
+    with pytest.raises(Exception):
+        elementwise_eor(a, b)
+
+
+def test_elementwise_xor():
+    a = Grid([[0, 0], [3, 4]])
+    b = Grid([[0, 2], [0, 4]])
+
+    assert elementwise_xor(a, b) == Grid([[0, 2], [3, 0]])
