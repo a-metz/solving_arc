e678fdb375587ec3c765a16d30b1877969327b07
Alexander Metz <alexander.wt.metz@gmail.com>
2020-03-05 22:56:17 +0100

    add island discovery operation

--- language/segmentation.py
+++ language/segmentation.py
@@ -5,3 +5,44 @@ from .grid import Grid
 
 def extract_rectangle(grid, origin, shape):
     return grid[origin[0] : shape[0] - origin[0], origin[1] : shape[1] - origin[1]]
+
+
+def _neighbors(index):
+    y, x = index
+    return [
+        (y - 1, x - 1),
+        (y - 1, x),
+        (y - 1, x + 1),
+        (y, x - 1),
+        (y, x + 1),
+        (y + 1, x - 1),
+        (y + 1, x),
+        (y + 1, x + 1),
+    ]
+
+
+def extract_islands(grid, water=0):
+    unassigned = {index for index, value in np.ndenumerate(grid.state) if value != water}
+
+    def discover_island_bounds(index):
+        top, left = index
+        bottom, right = index[0] + 1, index[1] + 1
+
+        for neighbor in _neighbors(index):
+            if neighbor in unassigned:
+                unassigned.remove(neighbor)
+                top_, left_, bottom_, right_ = discover_island_bounds(neighbor)
+                top = min(top, top_)
+                left = min(left, left_)
+                bottom = max(bottom, bottom_)
+                right = max(right, right_)
+
+        return top, left, bottom, right
+
+    islands = []
+    while len(unassigned) > 0:
+        top, left, bottom, right = discover_island_bounds(unassigned.pop())
+        island = Grid(grid.state[top:bottom, left:right])
+        islands.append(island)
+
+    return islands
--- language/test_segmentation.py
+++ language/test_segmentation.py
@@ -12,3 +12,59 @@ def filled_grid():
 def test_extract_rectangle(filled_grid):
     extracted_grid = Grid([[1, 2], [4, 5], [7, 8]])
     assert extract_rectangle(filled_grid, origin=(0, 0), shape=(3, 2)) == extracted_grid
+
+
+@pytest.fixture
+def islands_grid():
+    """ A grid with some islands
+    - with diagonal connection
+    - with different and same values
+    - without overlapping bounding boxes
+    - with and without bordering the grid edge
+    """
+    return Grid.from_string(
+        """
+        1 0 1 0 0 0
+        0 1 1 0 2 2
+        0 0 0 0 2 2
+        0 1 0 0 2 2
+        0 1 1 0 0 0
+        0 0 1 1 0 0
+        0 0 0 0 0 0
+        """
+    )
+
+
+def test_extract_islands(islands_grid):
+    islands = extract_islands(islands_grid)
+
+    assert len(islands) == 3
+    assert (
+        Grid.from_string(
+            """
+            1 0 1
+            0 1 1
+            """
+        )
+        in islands
+    )
+    assert (
+        Grid.from_string(
+            """
+            2 2
+            2 2
+            2 2
+            """
+        )
+        in islands
+    )
+    assert (
+        Grid.from_string(
+            """
+            1 0 0
+            1 1 0
+            0 1 1
+            """
+        )
+        in islands
+    )
