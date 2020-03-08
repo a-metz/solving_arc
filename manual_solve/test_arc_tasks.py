f7a9bb0c77823fe240e3271a39d20b3c7489257c
Alexander Metz <alexander.wt.metz@gmail.com>
2020-03-08 22:35:34 +0100

    solve an arc task manually

--- manual_solve/test_arc_tasks.py
+++ manual_solve/test_arc_tasks.py
@@ -9,3 +9,11 @@ def test_0520fde7(input_, expected):
     merged = elementwise_eand(*islands)
     result = switch_color(merged, 1, 2)
     assert result == expected
+
+
+def test_0d3d703e(input_, expected):
+    result = switch_color(input_, 3, 4)
+    result = switch_color(result, 1, 5)
+    result = switch_color(result, 2, 6)
+    result = switch_color(result, 8, 9)
+    assert result == expected
