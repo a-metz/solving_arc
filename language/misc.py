2949cbc649e4e3b301b25f523f27a2c9100b0584
Alexander Metz <alexander.wt.metz@gmail.com>
2020-03-13 18:42:55 +0100

    parameterize swap color

--- language/misc.py
+++ language/misc.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from .grid import Grid
 
 
@@ -16,3 +18,19 @@ def switch_color(grid, a, b):
     mapped.state[grid.state == a] = b
     mapped.state[grid.state == b] = a
     return mapped
+
+
+def _parameterize_switch_color(grid):
+    """partially apply switch color with sensible parameter combinations"""
+    valid_colors = range(10)
+    parameterized = []
+    for source_color in grid.used_colors():
+        for target_color in valid_colors:
+            if source_color == target_color:
+                continue
+            parameterized.append(partial(switch_color, a=source_color, b=target_color))
+
+    return parameterized
+
+
+switch_color.parameterize = _parameterize_switch_color
--- language/test_misc.py
+++ language/test_misc.py
@@ -23,3 +23,33 @@ def test_switch_color(example_grid):
     expected_grid = Grid([[2, 1, 3], [4, 5, 6], [7, 8, 9]])
 
     assert switch_color(example_grid, 1, 2) == expected_grid
+
+
+def test_switch_color_parameterize():
+    grid = Grid([[1, 3]])
+    switch_color_functions = switch_color.parameterize(grid)
+
+    # for each source color (1 and 3) to all 9 other colors
+    assert len(switch_color_functions) == 2 * 9
+
+    results = {func(grid) for func in switch_color_functions}
+
+    assert results == {
+        Grid([[0, 3]]),
+        Grid([[2, 3]]),
+        Grid([[3, 1]]),  # may be returned mutiple times
+        Grid([[4, 3]]),
+        Grid([[5, 3]]),
+        Grid([[6, 3]]),
+        Grid([[7, 3]]),
+        Grid([[8, 3]]),
+        Grid([[9, 3]]),
+        Grid([[1, 0]]),
+        Grid([[1, 2]]),
+        Grid([[1, 4]]),
+        Grid([[1, 5]]),
+        Grid([[1, 6]]),
+        Grid([[1, 7]]),
+        Grid([[1, 8]]),
+        Grid([[1, 9]]),
+    }
