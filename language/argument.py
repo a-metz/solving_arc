17cb5ea7308c73416e9a18d70e2018c2c059ca49
Alexander Metz <alexander.wt.metz@gmail.com>
2020-03-14 12:40:43 +0100

    avoid shadowing

--- language/argument.py
+++ language/argument.py
@@ -5,23 +5,23 @@ class ArgumentError(Exception):
     pass
 
 
-def extract_scalar(arg, type_=Grid):
-    if isinstance(arg, type_):
-        return arg
+def extract_scalar(argument, type_=Grid):
+    if isinstance(argument, type_):
+        return argument
 
     # might be packed in single element list
-    if hasattr(arg, "__len__") and len(arg) == 1:
-        return extract_scalar(arg[0])
+    if hasattr(argument, "__len__") and len(argument) == 1:
+        return extract_scalar(argument[0])
 
     raise ArgumentError("expected single element of correct type")
 
 
-def extract_tuple(arg, length=2, type_=Grid):
-    if hasattr(arg, "__len__") and len(arg) == length:
-        return tuple(extract_scalar(arg, type_) for arg in arg)
+def extract_tuple(argument, length=2, type_=Grid):
+    if hasattr(argument, "__len__") and len(argument) == length:
+        return tuple(extract_scalar(arg, type_) for arg in argument)
 
     # might be packed in another single element list
-    if hasattr(arg, "__len__") and len(arg) == 1:
-        return extract_tuple(arg[0], length)
+    if hasattr(argument, "__len__") and len(argument) == 1:
+        return extract_tuple(argument[0], length)
 
     raise ArgumentError("expected correct number of elements of correct type")
