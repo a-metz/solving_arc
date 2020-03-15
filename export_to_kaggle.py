ee8798b9a1c9cfd2f14e10f6bea7ebce2a2205b6
Alexander Metz <alexander.wt.metz@gmail.com>
2020-03-15 01:14:24 +0100

    fiddle around with kaggle limitations

--- export_to_kaggle.py
+++ export_to_kaggle.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 output = Path.cwd().joinpath("kaggle.py").open("w")
 output.write("from pathlib import Path\n\n")
+output.write("path = Path.cwd()\n")
+output.write("print('generating files to', path)\n")
 
 for input_path in Path.cwd().joinpath("solve_arc").glob("**/*.py"):
     if input_path.name[:5] == "test_":
@@ -11,11 +13,9 @@ for input_path in Path.cwd().joinpath("solve_arc").glob("**/*.py"):
     output.write("\n")
     output.write("# {}\n".format(relative_path))
     output.write(
-        "Path.cwd().joinpath('{}').mkdir(parents=True, exist_ok=True)\n".format(
-            relative_path.parent
-        )
+        "path.joinpath('{}').mkdir(parents=True, exist_ok=True)\n".format(relative_path.parent)
     )
-    output.write("open('{}', 'w').write(".format(relative_path))
+    output.write("path.joinpath('{}').open('w').write(".format(relative_path))
     output.write(repr(input_path.read_text()))
     output.write(")\n")
 
