7c2a773c14997d5c7fb25d8d080e739f689ae2dd
Alexander Metz <alexander.wt.metz@gmail.com>
2020-03-21 11:51:59 +0100

    multi constraint without logic

--- solve_arc/solver/sampling_search.py
+++ solve_arc/solver/sampling_search.py
@@ -1,5 +1,5 @@
-from itertools import chain, product, combinations
-from functools import partial
+from itertools import chain, product, combinations, repeat
+from functools import partial, wraps
 from collections import namedtuple
 import logging
 
@@ -13,18 +13,22 @@ Constraint = namedtuple("Constraint", ["source", "target"])
 
 
 def solve(constraints, max_depth):
-    source, target = constraints[0]
+    sources, targets = zip(*constraints)
 
-    source_function = Source(source)
-    if source_function.value == target:
+    source_function = Source(sources)
+    if source_function.value == targets:
         return Solution(source_function, source_function)
 
     leafs = [source_function]
     for step in range(max_depth):
-        leafs = valid_functions(leafs, target)
+        leafs = valid_functions(leafs, targets)
         for leaf in leafs:
-            if not hasattr(leaf.value, "__len__") and leaf.value == target:
+            print(leaf.value)
+            if leaf.value == targets:
                 return Solution(leaf, source_function)
+
+    print("No Solution")
+    return None
 
 
 class Function:
@@ -34,14 +38,14 @@ class Function:
         self.operation = operation
         self.args = args
         self.kwargs = kwargs
+        print(repr(self))
 
     @property
     def value(self):
-        """evaluate function with cached args and update cache"""
+        """evaluate function with cached arg values and cache result"""
 
         arg_values = [arg.value for arg in self.args]
-        kwarg_values = {key: arg.value for key, arg in self.kwargs.items()}
-        value = self.operation(*arg_values, **kwarg_values)
+        value = self.operation(*arg_values)
 
         # implement caching by overwriting property value for next call
         # (cached_property package is not available on kaggle docker image)
@@ -50,31 +54,64 @@ class Function:
         return value
 
     def __call__(self):
-        """evaluate function with real args"""
-        real_args = [arg() for arg in self.args]
-        real_kwargs = {key: arg() for key, arg in self.kwargs.items()}
-        return self.operation(*real_args, **real_kwargs)
+        """reevaluate function with reevaluated args"""
+        args = [arg() for arg in self.args]
+        return self.operation(*args)
 
     def __str__(self):
-        return "{}({})".format(self.operation.__name__, ", ".join(self.format_arguments(str)))
+        return "{}({})".format(self.operation.__name__, ", ".join(str(arg) for arg in self.args))
 
     def __repr__(self):
         return "Function({})".format(
-            ", ".join([self.operation.__name__] + self.format_arguments(repr))
+            ", ".join([self.operation.__name__] + [repr(arg) for arg in self.args])
         )
-
-    def format_arguments(self, value_formatter):
-        args_strings = [value_formatter(arg) for arg in self.args]
-        kwargs_strings = [
-            "{}={}".format(key, value_formatter(value)) for key, value in self.kwargs.items()
-        ]
-        return args_strings + kwargs_strings
 
     # def __eq__(self, other):
     #     return hash(self) == hash(other)
 
     # def __hash__(self):
     #     return hash(operation) ^ hash(tuple(self.args)) ^ hash(tuple(self.kwargs.items()))
+
+
+# class GridTuple(tuple):
+#     """wraps grids into tuple for solving multiple contraints
+
+#     tuple elements are of type Grid, optionally packed in nested lists [Grid, [Grid],...]
+#     """
+
+#     def is_scalar(self):
+#         """all value tuple elements are not nested in containers"""
+#         return all(isinstance(value, Grid) for value in self)
+
+#     def used_colors(self):
+#         """intersection of used colors in value tuple elements"""
+#         used_colors = (set(grid.used_colors()) for grid in walk(self))
+#         return tuple([set.intersection(*used_colors)] * len(self))
+
+#     @property
+#     def shape(self):
+#         assert self.is_scalar()
+#         return tuple(grid.shape for grid in self)
+
+
+# def walk(root):
+#     """depth first walk through nested iterables"""
+#     if hasattr(root, "__len__"):
+#         for child in root:
+#             yield from walk(child)
+#     else:
+#         yield root
+
+
+def vectorize(func):
+    """vectorize function for elements in tuple of first argument"""
+
+    # TODO: vectorize over multiple arguments (argument on decorator creation?)
+    @wraps(func)
+    def wrapper(*arg_tuples):
+        return tuple(func(*args) for args in zip(*arg_tuples))
+
+    return wrapper
 
 
 class Source:
@@ -107,8 +144,8 @@ class Source:
 class Constant:
     """cached value constant"""
 
-    def __init__(self, value=None):
-        self.value = value
+    def __init__(self, scalar=None):
+        self.value = repeat(scalar)
 
     def __call__(self):
         """reevaluate input after loading"""
@@ -121,11 +158,11 @@ class Constant:
         # return str(self)
         return "Constant({})".format(repr(self.value))
 
-    def __eq__(self, other):
-        return hash(self) == hash(other)
+    # def __eq__(self, other):
+    #     return hash(self) == hash(other)
 
-    def __hash__(self):
-        return hash(self.value)
+    # def __hash__(self):
+    #     return hash(self.value)
 
 
 class Solution:
@@ -134,8 +171,9 @@ class Solution:
         self.source = source
 
     def __call__(self, value):
-        self.source.load(value)
-        return self.function()
+        # run only for single element
+        self.source.load((value,))
+        return self.function()[0]
 
     def __str__(self):
         return str(self.function)
@@ -151,13 +189,13 @@ def valid_functions(args, target):
         + extract_islands_functions(args, target)
         + extract_color_patches_functions(args, target)
         + extract_color_patch_functions(args, target)
-        + logic_functions(args, target)
+        # + logic_functions(args, target)
     )
 
 
 def map_color_functions(args, target):
     return [
-        Function(map_color, arg, from_color, to_color)
+        Function(vectorize(map_color), arg, Constant(from_color), Constant(to_color),)
         for arg in scalar_grids(args)
         for from_color, to_color in product(
             used_colors(arg.value),
@@ -168,7 +206,7 @@ def map_color_functions(args, target):
 
 def swap_color_functions(args, target):
     return [
-        Function(switch_color, arg, a, b)
+        Function(vectorize(switch_color), arg, Constant(a), Constant(b),)
         for arg in scalar_grids(args)
         for a, b in combinations(used_colors(arg.value), 2)
     ]
@@ -176,67 +214,78 @@ def swap_color_functions(args, target):
 
 def extract_islands_functions(args, target):
     return [
-        Function(extract_islands, arg, ignore=color)
+        Function(vectorize(extract_islands), arg, Constant(color))
         for arg in scalar_grids(args)
-        if arg.value.shape != target.shape  # heuristic: if target has different shape
+        if shape(arg.value) != shape(target)  # heuristic: if target has different shape
         for color in used_colors(arg.value)
     ]
 
 
 def extract_color_patches_functions(args, target):
     return [
-        Function(extract_color_patches, arg, ignore=color)
+        Function(vectorize(extract_color_patches), arg, Constant(color))
         for arg in scalar_grids(args)
-        if arg.value.shape != target.shape  # heuristic: if target has different shape
+        if shape(arg.value) != shape(target)  # heuristic: if target has different shape
         for color in used_colors(arg.value)
     ]
 
 
 def extract_color_patch_functions(args, target):
     return [
-        Function(extract_color_patch, arg, color)
+        Function(vectorize(extract_color_patch), arg, Constant(color))
         for arg in scalar_grids(args)
-        if arg.value.shape != target.shape  # heuristic: if target has different shape
+        if shape(arg.value) != shape(target)  # heuristic: if target has different shape
         for color in used_colors(arg.value)
     ]
 
 
-def logic_functions(args, _):
-    functions = []
-    for a, b in unpack(shape_matching_grid_pairs(args), 2):
-        functions.append(Function(elementwise_equal_and, a, b))
-        functions.append(Function(elementwise_equal_or, a, b))
-        functions.append(Function(elementwise_xor, a, b))
-    return functions
+# def logic_functions(args, _):
+#     functions = []
+#     for a, b in unpack(shape_matching_grid_pairs(args), 2):
+#         functions.append(Function(vectorize(elementwise_equal_and), a, b))
+#         functions.append(Function(vectorize(elementwise_equal_or), a, b))
+#         functions.append(Function(vectorize(elementwise_xor), a, b))
+#     return functions
 
 
 def scalar_grids(args):
-    return [arg for arg in args if isinstance(arg.value, Grid)]
+    """filter args for grid tuples which are not nested in containers"""
+    return [arg for arg in args if is_scalar(arg.value)]
 
 
-def used_colors(grid):
-    return [Constant(color) for color in grid.used_colors()]
+def is_scalar(grid_tuple):
+    return all(isinstance(grid, Grid) for grid in grid_tuple)
 
 
-def shape_matching_grid_pairs(args):
-    # TODO: also enumerate all combinations of two scalar grids with same shape
-    return [
-        arg
-        for arg in args
-        if hasattr(arg.value, "__len__")
-        and len(arg.value) == 2
-        and isinstance(arg.value[0], Grid)
-        and isinstance(arg.value[1], Grid)
-        and arg.value[0].shape == arg.value[1].shape
-    ]
+def used_colors(grid_tuple):
+    """intersection of used colors in value tuple elements"""
+    used_colors = (set(grid.used_colors()) for grid in grid_tuple)
+    return set.intersection(*used_colors)
 
 
-def unpack(args, num_elements):
-    return [[Function(get_item(index), arg) for index in range(num_elements)] for arg in args]
+def shape(grid_tuple):
+    return tuple(grid.shape for grid in grid_tuple)
 
 
-def get_item(index):
-    # TODO: implement as class with hash function dependent on only on argument
-    function = lambda container: container[index]
-    function.__name__ = "get_item_{}".format(index)
-    return function
+# def shape_matching_grid_pairs(args):
+#     # TODO: also enumerate all combinations of two scalar grids with same shape
+#     return [
+#         arg
+#         for arg in args
+#         if hasattr(arg.value, "__len__")
+#         and len(arg.value) == 2
+#         and is_scalar(arg.value[0], Grid)
+#         and is_scalar(arg.value[1], Grid)
+#         and arg.value[0].shape == arg.value[1].shape
+#     ]
+
+
+# def unpack(args, num_elements):
+#     return [[Function(get_item(index), arg) for index in range(num_elements)] for arg in args]
+
+
+# def get_item(index):
+#     # TODO: implement as class with hash function dependent on only on argument
+#     function = lambda container: container[index]
+#     function.__name__ = "get_item_{}".format(index)
+#     return function
--- solve_arc/solver/test_sampling_search.py
+++ solve_arc/solver/test_sampling_search.py
@@ -8,7 +8,7 @@ from ..language import *
 # logging.getLogger().setLevel(logging.DEBUG)
 
 
-def test_solve_nop():
+def test_solve__nop():
     source = Grid([[1, 2, 3]])
     target = source
     constraints = [Constraint(source, target)]
@@ -16,32 +16,73 @@ def test_solve_nop():
     solution = solve(constraints, max_depth=4)
 
     # identity
+    assert solution is not None
     test = Grid([[4, 5, 6]])
     assert solution(test) == test
 
 
-def test_solve_no_solution_within_depth():
+def test_solve__no_solution_within_depth():
     source = Grid([[1, 2, 3]])
     target = Grid([[4, 5, 6]])
     constraints = [Constraint(source, target)]
 
     solution = solve(constraints, max_depth=2)
 
-    assert solution == None
+    assert solution is None
 
 
-def test_solve_simple_colorswap():
-    source = Grid([[1, 2]])
-    target = Grid([[2, 1]])
+def test_solve__single_colorswap():
+    source = Grid([[1, 2, 2]])
+    target = Grid([[2, 1, 1]])
     constraints = [Constraint(source, target)]
 
     solution = solve(constraints, max_depth=1)
 
     assert solution is not None
     assert solution(source) == target
+    assert solution(Grid([[1, 1, 2]])) == Grid([[2, 2, 1]])
 
 
-def test_solve_complex():
+@pytest.mark.skip
+def test_solve__single_xor():
+    source = (Grid([[1, 0, 0]]), Grid([[1, 1, 0]]))
+    target = Grid([[0, 1, 0]])
+    constraints = [Constraint(source, target)]
+
+    solution = solve(constraints, max_depth=1)
+
+    assert solution is not None
+    assert solution(source) == target
+    assert solution((Grid([[1, 0, 1]]), Grid([[1, 1, 0]]))) == Grid([[0, 1, 1]])
+
+
+def test_solve__single_colorswap__multiple_contraints():
+    constraints = [
+        Constraint(source=Grid([[1, 2, 2]]), target=Grid([[2, 1, 1]])),
+        Constraint(source=Grid([[1, 2, 1]]), target=Grid([[2, 1, 2]])),
+    ]
+
+    solution = solve(constraints, max_depth=1)
+
+    assert solution is not None
+    assert solution(Grid([[1, 1, 2]])) == Grid([[2, 2, 1]])
+
+
+@pytest.mark.skip
+def test_solve__single_xor__multiple_contraints():
+    constraints = [
+        Constraint(source=(Grid([[1, 0, 0]]), Grid([[1, 1, 0]])), target=Grid([[0, 1, 0]])),
+        Constraint(source=(Grid([[0, 1, 1]]), Grid([[1, 1, 0]])), target=Grid([[1, 0, 1]])),
+    ]
+
+    solution = solve(constraints, max_depth=1)
+
+    assert solution is not None
+    assert solution((Grid([[1, 0, 1]]), Grid([[1, 1, 0]]))) == Grid([[0, 1, 1]])
+
+
+@pytest.mark.skip
+def test_solve__complex():
     source = Grid([[0, 0, 1], [2, 2, 2], [1, 0, 1]])
     target = Grid([[3, 0, 3]])
     constraints = [Constraint(source, target)]
@@ -49,22 +90,11 @@ def test_solve_complex():
     solution = solve(constraints, max_depth=3)
 
     assert solution is not None
-    assert solution(source) == target
-
-
-def test_solve_and_transfer():
-    source = Grid([[0, 0, 1], [2, 2, 2], [1, 0, 1]])
-    target = Grid([[3, 0, 3]])
-    constraints = [Constraint(source, target)]
-
-    solution = solve(constraints, max_depth=3)
-
-    test = Grid([[1, 1, 0, 0], [2, 2, 2, 2], [1, 0, 0, 1]])
-    assert solution(test) == Grid([[3, 3, 0, 3]])
+    assert solution(Grid([[1, 1, 0, 0], [2, 2, 2, 2], [1, 0, 0, 1]])) == Grid([[3, 3, 0, 3]])
 
 
 @pytest.mark.skip
-def test_solve_multiple_constraints_and_transfer():
+def test_solve__complex__multiple_constraints():
     # undefined / no color swap
     constraint_1 = Constraint(source=Grid([[1, 0], [2, 2], [1, 0]]), target=Grid([[0, 0]]))
 
