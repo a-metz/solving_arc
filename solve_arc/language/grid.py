cca31f5272eb73f0d6b6e0058f5acb2c7146d40a
Alexander Metz <alexander.wt.metz@gmail.com>
2020-03-25 22:06:45 +0100

    add mask type

--- solve_arc/language/grid.py
+++ solve_arc/language/grid.py
@@ -1,32 +1,23 @@
 import numpy as np
 
 
-class Grid:
+class _Base:
     def __init__(self, state):
         self.state = np.array(state, copy=True)
 
-        assert self.state.dtype.kind in ["i", "u"], "state datatype must be integral"
         assert len(self.state.shape) == 2, "state needs to be 2d array"
-
-    @classmethod
-    def empty(cls, shape):
-        return cls(np.zeros(shape=shape, dtype=np.int))
 
     @classmethod
     def from_string(cls, string):
         lines = [line.strip() for line in string.splitlines() if len(line.strip()) > 0]
         elements = [[int(element) for element in line.split()] for line in lines]
-        return Grid(elements)
+        return cls(elements)
 
     def copy(self):
         return self.__class__(self.state)
 
     def enumerate(self):
         return np.ndenumerate(self.state)
-
-    def used_colors(self):
-        """returns list of colors used in grid"""
-        return np.unique(self.state)
 
     @property
     def shape(self):
@@ -48,8 +39,55 @@ class Grid:
     def __eq__(self, other):
         return isinstance(other, self.__class__) and hash(self) == hash(other)
 
+    def __repr__(self):
+        return "{}({})".format(self.__class__, self.state.tolist())
+
+
+class Grid(_Base):
+    def __init__(self, state):
+        super().__init__(state)
+
+        assert self.state.dtype.kind in ["i", "u"], "state datatype must be integral"
+
+    @classmethod
+    def empty(cls, shape):
+        return cls(np.zeros(shape=shape, dtype=np.int))
+
+    @classmethod
+    def from_string(cls, string):
+        elements = [[int(char) for char in line] for line in filtered_elements(string)]
+        return cls(elements)
+
+    def used_colors(self):
+        """returns list of colors used in grid"""
+        return np.unique(self.state)
+
     def __str__(self):
         return "\n".join([" ".join([str(char) for char in row]) for row in self.state])
 
-    def __repr__(self):
-        return "Grid({})".format(repr(self.state.tolist()))
+
+class Mask(_Base):
+    def __init__(self, state):
+        super().__init__(state)
+
+        assert self.state.dtype.kind == "b", "state datatype must be boolean"
+
+    @classmethod
+    def empty(cls, shape):
+        return cls(np.zeros(shape=shape, dtype=np.bool))
+
+    @classmethod
+    def from_string(cls, string):
+        elements = [[char == "#" for char in line] for line in filtered_elements(string)]
+        return cls(elements)
+
+    def any(self):
+        """returns whether any element is true in mask"""
+        return np.any(self.state)
+
+    def __str__(self):
+        return "\n".join([" ".join(["#" if value else "." for value in row]) for row in self.state])
+
+
+def filtered_elements(string):
+    return (line.strip().split() for line in string.splitlines() if len(line.strip()) > 0)
--- solve_arc/language/test_grid.py
+++ solve_arc/language/test_grid.py
@@ -3,14 +3,14 @@ import pytest
 from .grid import *
 
 
-def test_create_empty():
+def test_grid_create_empty():
     grid = Grid.empty(shape=(5, 5))
 
     assert grid.shape == (5, 5)
     assert grid[0, 0] == 0
 
 
-def test_create():
+def test_grid_create():
     grid = Grid([[1, 2], [3, 4]])
 
     assert grid.shape == (2, 2)
@@ -18,29 +18,37 @@ def test_create():
     assert grid[1, 0] == 3
 
 
-def test_create__1darray():
+def test_grid_create__1darray():
     with pytest.raises(AssertionError):
         grid = Grid([1, 2])
 
 
-def test_create__ndarray():
+def test_grid_create__ndarray():
     with pytest.raises(AssertionError):
         grid = Grid([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
 
 
-def test_equality():
+def test_grid_create__non_integer():
+    with pytest.raises(AssertionError):
+        grid = Grid([True, False])
+
+    with pytest.raises(AssertionError):
+        grid = Grid([1.0, 2.0])
+
+
+def test_grid_equality():
     a = Grid([[1, 2], [3, 4]])
     b = Grid([[1, 2], [3, 4]])
     assert a == b
 
 
-def test_inequality():
+def test_grid_inequality():
     a = Grid([[1, 2], [3, 4]])
     b = Grid([[0, 2], [3, 4]])
     assert a != b
 
 
-def test_create_from_string():
+def test_grid_create_from_string():
     string = """
     1 2
     3 4
@@ -50,14 +58,14 @@ def test_create_from_string():
     assert grid == Grid([[1, 2], [3, 4]])
 
 
-def test_string_representation():
+def test_grid_string_representation():
     grid = Grid([[1, 2], [3, 4]])
     expected_string = "1 2\n3 4"
 
     assert str(grid) == expected_string
 
 
-def test_copy():
+def test_grid_copy():
     grid = Grid([[1, 2], [3, 4]])
 
     copy = grid.copy()
@@ -67,7 +75,7 @@ def test_copy():
     assert copy != grid
 
 
-def test_enumerate():
+def test_grid_enumerate():
     grid = Grid([[1, 2], [3, 4]])
 
     assert len(list(grid.enumerate())) == 4
@@ -75,10 +83,56 @@ def test_enumerate():
         assert grid[index] == value
 
 
-def test_used_colors():
+def test_grid_used_colors():
     grid = Grid([[1, 2], [1, 4]])
 
     used_colors = grid.used_colors()
     expected_used_colors = {1, 2, 4}
     assert len(used_colors) == len(expected_used_colors)
     assert set(used_colors) == expected_used_colors
+
+
+def test_mask_create_empty():
+    mask = Mask.empty(shape=(5, 5))
+
+    assert mask.shape == (5, 5)
+    assert not mask[0, 0]
+
+
+def test_mask_create():
+    mask = Mask([[1, 2], [3, 4]])
+
+    assert mask.shape == (2, 2)
+    assert mask[0, 0] == 1
+    assert mask[1, 0] == 3
+
+
+def test_mask_create__non_boolean():
+    with pytest.raises(AssertionError):
+        mask = Mask([1, 2])
+
+    with pytest.raises(AssertionError):
+        mask = Mask([1.0, 2.0])
+
+
+def test_mask_any():
+    assert Mask([[True, False]]).any()
+    assert not Mask([[False, False]]).any()
+
+
+def test_mask_create_from_string():
+    string = """
+    # #
+    # .
+    """
+
+    mask = Mask.from_string(string)
+
+    assert mask == Mask([[True, True], [True, False]])
+
+
+def test_mask_string_representation():
+    mask = Mask([[True, True], [True, False]])
+    expected_string = "# #\n# ."
+
+    assert str(mask) == expected_string
