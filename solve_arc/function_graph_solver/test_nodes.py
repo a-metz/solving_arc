819b7b1c00af55acbdef7b3033e560c66aa9bc4b
Alexander Metz <alexander.wt.metz@gmail.com>
2020-04-04 19:12:29 +0200

    test hashing

--- solve_arc/function_graph_solver/test_nodes.py
+++ solve_arc/function_graph_solver/test_nodes.py
@@ -31,11 +31,44 @@ def test_function__lazy_evaluation_with_multiple_arguments():
     def add_second_times_three(a, b):
         return a + (3 * b)
 
-    arg_1 = Constant(0)
-    arg_2 = Constant(0)
-    function = Function(add_second_times_three, arg_1, arg_2)
+    arg_a = Constant(0)
+    arg_b = Constant(0)
+    function = Function(add_second_times_three, arg_a, arg_b)
 
     # modify constant value after creating function to check lazy evaluation
-    arg_1.value = 2
-    arg_2.value = 5
+    arg_a.value = 2
+    arg_b.value = 5
     assert function() == 2 + (3 * 5)
+
+
+def test_constant__hash():
+    assert hash(Constant(0)) != hash(Constant(1))
+
+    # stress it a bit as function wrappers can also coincidentally have the same hash
+    for i in range(10):
+        assert hash(Constant(i)) == hash(Constant(i))
+
+
+def test_function__hash_equality():
+    def func(*args):
+        pass
+
+    arg = Constant(0)
+
+    assert hash(Function(func)) == hash(Function(func))
+    assert hash(Function(func, arg)) == hash(Function(func, arg))
+
+
+def test_function__hash_inequality():
+    def func_a(a, b):
+        pass
+
+    def func_b(a, b):
+        pass
+
+    arg_a = Constant(0)
+    arg_b = Constant(1)
+
+    assert hash(Function(func_a)) != hash(Function(func_b))
+    assert hash(Function(func_a, arg_a)) != hash(Function(func_b, arg_b))
+    assert hash(Function(func_a, arg_a, arg_b)) != hash(Function(func_b, arg_b, arg_a))
