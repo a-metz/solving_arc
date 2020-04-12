94a360a9b28ab6afdf63f7e79fa99f3fd4cd255d
Alexander Metz <alexander.wt.metz@gmail.com>
2020-04-12 14:52:14 +0200

    refactor function generation for sampling search

--- solve_arc/function_graph_solver/function_generation.py
+++ solve_arc/function_graph_solver/function_generation.py
@@ -18,94 +18,108 @@ class Graph:
         self.target = target
         self.max_depth = max_depth
 
-        # nodes by type for faster access
-        self.nodes_type = NodesByType()
         # all nodes that have been generated, for checking for new nodes
         self.nodes = set()
+
+        # nodes that have already been expanded
+        self.expanded_nodes = set()
+        self.expandable_nodes = set()
 
         self._process(initial_nodes)
 
     def expand(self):
-        return self._process(self.generate_functions() - self.nodes)
+        expand_next = NodeCollection(self.expandable_nodes)
+        self.expandable_nodes -= expand_next
+        logger.debug(
+            "expand %d nodes (Grid: %d, Grids: %d, Selection: %d, Selections: %d)",
+            len(expand_next),
+            len(expand_next.of_type(Grid)),
+            len(expand_next.of_type(Grids)),
+            len(expand_next.of_type(Selection)),
+            len(expand_next.of_type(Selections)),
+        )
+
+        if len(expand_next) == 0:
+            raise NoExpandableNodes()
+
+        new_nodes = generate_functions(expand_next, self) - self.nodes
+        self.expanded_nodes |= expand_next
+
+        return self._process(new_nodes)
 
     def _process(self, new_nodes):
         self.nodes |= new_nodes
+        self.expandable_nodes |= {
+            node for node in new_nodes if is_valid(node) and node.depth() < self.max_depth
+        }
+        logger.debug(
+            "new nodes: %d, total expandable: %d, total nodes: %d",
+            len(new_nodes),
+            len(self.expandable_nodes),
+            len(self.nodes),
+        )
 
         # check for solution
         for node in new_nodes:
             if node() == self.target:
                 return node
 
-        expandable = [
-            node for node in new_nodes if is_valid(node) and node.depth() < self.max_depth
-        ]
-
-        logger.debug(
-            "nodes: new: %d, expandable: %d, total: %d",
-            len(new_nodes),
-            len(expandable),
-            len(self.nodes),
-        )
-
-        if len(expandable) == 0:
-            raise NoExpandableNodes()
-
-        self.nodes_type.filter(expandable)
-
         return None
-
-    # TODO: switch completely to selection based functions(?)
-    # TODO: write unpack function which unpacks first, second, last(?), largest, smallest, tallest, widest, ... as new nodes
-    def generate_functions(self):
-        functions = (
-            select_color_functions(self)
-            | select_all_colors_functions(self)
-            | extract_selected_area_functions(self)
-            | extract_selected_areas_functions(self)
-            | set_selected_to_color_functions(self)
-            | merge_selections_functions(self)
-            | filter_selections_functions(self)
-            | split_selection_into_connected_areas_functions(self)
-            | switch_color_functions(self)
-            | map_color_functions(self)
-            | logic_functions(self)
-            | symmetry_functions(self)
-            | concatenate_functions(self)
-            | split_functions(self)
-            | extract_islands_functions(self)
-            | extract_color_patches_functions(self)
-            | extract_color_patch_functions(self)
-        )
-        return functions
 
 
 class NoExpandableNodes(Exception):
     pass
 
 
-class NodesByType:
-    def __init__(self):
-        self.by_type = defaultdict(set)
+class NodeCollection(set):
+    def __init__(self, iterable):
+        super().__init__(iterable)
+        self._filter_by_type(iterable)
 
-    def filter(self, nodes):
+    def _filter_by_type(self, nodes):
+        by_type = defaultdict(set)
         for node in nodes:
             vector_iter = iter(node())
             type_ = type(next(vector_iter))
             # only valid if all are equal
             if all(type_ == type(element) for element in vector_iter):
-                self.by_type[type_].add(node)
+                by_type[type_].add(node)
 
-        for type_, nodes in self.by_type.items():
-            logger.debug("type %s nodes: %d", type_.__name__, len(nodes))
+        self._by_type = by_type
 
-    def __call__(self, type_):
-        return self.by_type[type_]
+    def of_type(self, type_):
+        return self._by_type[type_]
 
 
-def map_color_functions(graph):
+# TODO: switch completely to selection based functions(?)
+# TODO: write unpack function which unpacks first, second, last(?), largest, smallest, tallest, widest, ... as new nodes
+def generate_functions(nodes, graph):
+    functions = (
+        select_color_functions(nodes, graph)
+        | select_all_colors_functions(nodes, graph)
+        | extract_selected_area_functions(nodes, graph)
+        | extract_selected_areas_functions(nodes, graph)
+        | set_selected_to_color_functions(nodes, graph)
+        | merge_selections_functions(nodes, graph)
+        | filter_selections_functions(nodes, graph)
+        | split_selection_into_connected_areas_functions(nodes, graph)
+        | switch_color_functions(nodes, graph)
+        | map_color_functions(nodes, graph)
+        | logic_functions(nodes, graph)
+        | symmetry_functions(nodes, graph)
+        | concatenate_functions(nodes, graph)
+        | split_functions(nodes, graph)
+        | extract_islands_functions(nodes, graph)
+        | extract_color_patches_functions(nodes, graph)
+        | extract_color_patch_functions(nodes, graph)
+    )
+    return functions
+
+
+def map_color_functions(nodes, graph):
     return {
         Function(vectorize(map_color), node, Constant(repeat(a)), Constant(repeat(b)))
-        for node in graph.nodes_type(Grid)
+        for node in nodes.of_type(Grid)
         for a, b in product(
             used_colors(node()),
             # heuristic: only map to colors used in target
@@ -115,88 +129,82 @@ def map_color_functions(graph):
     }
 
 
-def switch_color_functions(graph):
+def switch_color_functions(nodes, graph):
     return {
         Function(vectorize(switch_color), node, Constant(repeat(a)), Constant(repeat(b)))
-        for node in graph.nodes_type(Grid)
+        for node in nodes.of_type(Grid)
         if not (isinstance(node, Function) and node.callable_ == vectorize(switch_color))
         for a, b in combinations(used_colors(node()), 2)
     }
 
 
-def select_color_functions(graph):
+def select_color_functions(nodes, graph):
     return {
         Function(vectorize(select_color), node, Constant(repeat(color)))
-        for node in graph.nodes_type(Grid)
+        for node in nodes.of_type(Grid)
         for color in used_colors(node())
     }
 
 
-def select_all_colors_functions(graph):
+def select_all_colors_functions(nodes, graph):
     return {
         Function(vectorize(select_all_colors), node, Constant(repeat(color)))
-        for node in graph.nodes_type(Grid)
+        for node in nodes.of_type(Grid)
         for color in used_colors(node())
         # 2 colors or less is covered by select_color_functions
         if len(used_colors(node())) > 2
     }
 
 
-def split_selection_into_connected_areas_functions(graph):
+def split_selection_into_connected_areas_functions(nodes, graph):
     functions = set()
-    for node in graph.nodes_type(Selection):
+    for node in nodes.of_type(Selection):
         functions.add(Function(vectorize(split_selection_into_connected_areas), node))
         functions.add(Function(vectorize(split_selection_into_connected_areas_no_diagonals), node))
     return functions
 
 
-def filter_selections_functions(graph):
+def filter_selections_functions(nodes, graph):
     functions = set()
-    for node in graph.nodes_type(Selections):
+    for node in nodes.of_type(Selections):
         functions.add(Function(vectorize(filter_selections_touching_edge), node))
         functions.add(Function(vectorize(filter_selections_not_touching_edge), node))
     return functions
 
 
-def merge_selections_functions(graph):
+def merge_selections_functions(nodes, graph):
     return {
         Function(vectorize(merge_selections), selections)
-        for selections in graph.nodes_type(Selections)
+        for selections in nodes.of_type(Selections)
         if is_matching_shape(selections)
     }
 
 
-def set_selected_to_color_functions(graph):
+def set_selected_to_color_functions(nodes, graph):
     return {
         Function(
             vectorize(set_selected_to_color), grid_node, selection_node, Constant(repeat(color))
         )
-        for grid_node, selection_node in product(
-            graph.nodes_type(Grid), graph.nodes_type(Selection)
-        )
+        for grid_node, selection_node in product(nodes.of_type(Grid), nodes.of_type(Selection))
         if shape(grid_node()) == shape(selection_node())
         for color in used_colors(graph.target)
     }
 
 
-def extract_selected_area_functions(graph):
+def extract_selected_area_functions(nodes, graph):
     return {
         Function(vectorize(extract_selected_area), grid_node, selection_node)
-        for grid_node, selection_node in product(
-            graph.nodes_type(Grid), graph.nodes_type(Selection)
-        )
+        for grid_node, selection_node in product(nodes.of_type(Grid), nodes.of_type(Selection))
         if shape(grid_node()) == shape(selection_node())
         # heuristic: if target has different shape
         and shape(grid_node()) != shape(graph.target)
     }
 
 
-def extract_selected_areas_functions(graph):
+def extract_selected_areas_functions(nodes, graph):
     return {
         Function(vectorize(extract_selected_areas), grid_node, selections_node)
-        for grid_node, selections_node in product(
-            graph.nodes_type(Grid), graph.nodes_type(Selections)
-        )
+        for grid_node, selections_node in product(nodes.of_type(Grid), nodes.of_type(Selections))
         # can only process nodes ofs length 2 further
         if is_matching_shape(selections_node) and shape(grid_node()) == shape(selections_node()[0])
         # heuristic: if target has different shape
@@ -204,43 +212,43 @@ def extract_selected_areas_functions(graph):
     }
 
 
-def extract_islands_functions(graph):
+def extract_islands_functions(nodes, graph):
     return {
         Function(vectorize(extract_islands), node, Constant(repeat(color)))
-        for node in graph.nodes_type(Grid)
+        for node in nodes.of_type(Grid)
         # heuristic: if target has different shape
         if shape(node()) != shape(graph.target)
         for color in used_colors(node())
     }
 
 
-def extract_color_patches_functions(graph):
+def extract_color_patches_functions(nodes, graph):
     return {
         Function(vectorize(extract_color_patches), node, Constant(repeat(color)))
-        for node in graph.nodes_type(Grid)
+        for node in nodes.of_type(Grid)
         # heuristic: if target has different shape
         if shape(node()) != shape(graph.target)
         for color in used_colors(node())
     }
 
 
-def extract_color_patch_functions(graph):
+def extract_color_patch_functions(nodes, graph):
     return {
         Function(vectorize(extract_color_patch), node, Constant(repeat(color)))
-        for node in graph.nodes_type(Grid)
+        for node in nodes.of_type(Grid)
         # heuristic: if target has different shape
         if shape(node()) != shape(graph.target)
         for color in used_colors(node())
     }
 
 
-def concatenate_functions(graph):
+def concatenate_functions(nodes, graph):
     functions = set()
 
     # heuristic: only do concatenations for half target height
     half_height = (
         grid_node
-        for grid_node in graph.nodes_type(Grid)
+        for grid_node in nodes.of_type(Grid)
         if multiply(height(grid_node()), 2) == height(graph.target)
     )
     for top_node, bottom_node in combinations_with_replacement(half_height, 2):
@@ -250,7 +258,7 @@ def concatenate_functions(graph):
     # heuristic: only do concatenations for half target width
     half_width = (
         grid_node
-        for grid_node in graph.nodes_type(Grid)
+        for grid_node in nodes.of_type(Grid)
         if multiply(width(grid_node()), 2) == width(graph.target)
     )
     for left_node, right_node in combinations_with_replacement(half_width, 2):
@@ -260,10 +268,10 @@ def concatenate_functions(graph):
     return functions
 
 
-def split_functions(graph):
+def split_functions(nodes, graph):
     functions = set()
     # heuristic: only do concatenations for shape target.shape * num_splits
-    for grid_node in graph.nodes_type(Grid):
+    for grid_node in nodes.of_type(Grid):
         if height(grid_node()) == multiply(height(graph.target), 2):
             functions.add(Function(vectorize(split_top_bottom), grid_node))
         if height(grid_node()) == multiply(height(graph.target), 3):
@@ -277,9 +285,9 @@ def split_functions(graph):
 
 # TODO: also enumerate all combinations of two scalar grids with same shape
 # TODO: refactor logical functions to also take grid sequences (?)
-def logic_functions(graph):
+def logic_functions(nodes, graph):
     functions = set()
-    for sequence in graph.nodes_type(Grids):
+    for sequence in nodes.of_type(Grids):
         if is_matching_shape_pair(sequence):
             a = Function(get_item, sequence, Constant(repeat(0)))
             b = Function(get_item, sequence, Constant(repeat(1)))
@@ -289,9 +297,9 @@ def logic_functions(graph):
     return functions
 
 
-def symmetry_functions(graph):
+def symmetry_functions(nodes, graph):
     functions = set()
-    for node in graph.nodes_type(Grid):
+    for node in nodes.of_type(Grid):
         functions.add(Function(vectorize(flip_up_down), node))
         functions.add(Function(vectorize(flip_left_right), node))
         functions.add(Function(vectorize(rotate_90), node))
--- solve_arc/function_graph_solver/test_function_generation.py
+++ solve_arc/function_graph_solver/test_function_generation.py
@@ -6,19 +6,12 @@ from .nodes import Constant
 
 
 @pytest.fixture
-def target():
-    return repeat_once(
-        Grid.from_string(
-            """
-            1 2
-            1 1
-            """
-        )
-    )
+def invalid_node():
+    return Constant(repeat_once(None))
 
 
 @pytest.fixture
-def grid():
+def grid_node():
     return Constant(
         repeat_once(
             Grid.from_string(
@@ -33,7 +26,7 @@ def grid():
 
 
 @pytest.fixture
-def selection_1():
+def selection_node_1():
     return Constant(
         repeat_once(
             Selection.from_string(
@@ -48,7 +41,7 @@ def selection_1():
 
 
 @pytest.fixture
-def selection_2():
+def selection_node_2():
     return Constant(
         repeat_once(
             Selection.from_string(
@@ -63,7 +56,7 @@ def selection_2():
 
 
 @pytest.fixture
-def selection_wrong_size():
+def selection_node_wrong_size():
     return Constant(
         repeat_once(
             Selection.from_string(
@@ -77,25 +70,107 @@ def selection_wrong_size():
 
 
 @pytest.fixture
-def graph(target, grid, selection_1, selection_2, selection_wrong_size):
-    return Graph({grid, selection_1, selection_2, selection_wrong_size}, target, max_depth=1)
+def grids_node():
+    return Constant(
+        repeat_once(
+            Grids(
+                [
+                    Grid.from_string(
+                        """
+                        1 1
+                        1 0
+                        """
+                    ),
+                    Grid.from_string(
+                        """
+                        0 1
+                        1 0
+                        """
+                    ),
+                ]
+            )
+        )
+    )
 
 
-def test_extract_selected_area_functions(graph, grid, selection_1, selection_2):
-    functions = extract_selected_area_functions(graph)
+@pytest.fixture
+def selections_node():
+    return Constant(
+        repeat_once(
+            Selections(
+                [
+                    Selection.from_string(
+                        """
+                        # #
+                        # .
+                        """
+                    ),
+                    Selection.from_string(
+                        """
+                        . #
+                        # .
+                        """
+                    ),
+                ]
+            )
+        )
+    )
+
+
+def test_node_collections(
+    invalid_node, grid_node, grids_node, selection_node_1, selection_node_2, selections_node
+):
+    nodes = NodeCollection(
+        [invalid_node, grid_node, grids_node, selection_node_1, selection_node_2, selections_node]
+    )
+
+    assert len(nodes) == 6
+    assert nodes.of_type(Grid) == {grid_node}
+    assert nodes.of_type(Grids) == {grids_node}
+    assert nodes.of_type(Selection) == {selection_node_1, selection_node_2}
+    assert nodes.of_type(Selections) == {selections_node}
+
+
+@pytest.fixture
+def graph():
+    class Graph:
+        target = repeat_once(
+            Grid.from_string(
+                """
+                1 2
+                1 1
+                """
+            )
+        )
+
+    return Graph()
+
+
+def test_extract_selected_area_functions(graph, grid_node, selection_node_1, selection_node_2):
+    nodes = NodeCollection([grid_node, selection_node_1, selection_node_2])
+    functions = extract_selected_area_functions(nodes, graph)
 
     assert functions == {
-        Function(vectorize(extract_selected_area), grid, selection_1),
-        Function(vectorize(extract_selected_area), grid, selection_2),
+        Function(vectorize(extract_selected_area), grid_node, selection_node_1),
+        Function(vectorize(extract_selected_area), grid_node, selection_node_2),
     }
 
 
-def test_set_selected_to_color_functions(graph, grid, selection_1, selection_2):
-    functions = set_selected_to_color_functions(graph)
+def test_set_selected_to_color_functions(graph, grid_node, selection_node_1, selection_node_2):
+    nodes = NodeCollection([grid_node, selection_node_1, selection_node_2])
+    functions = set_selected_to_color_functions(nodes, graph)
 
     assert functions == {
-        Function(vectorize(set_selected_to_color), grid, selection_1, Constant(repeat_once(1))),
-        Function(vectorize(set_selected_to_color), grid, selection_2, Constant(repeat_once(1))),
-        Function(vectorize(set_selected_to_color), grid, selection_1, Constant(repeat_once(2))),
-        Function(vectorize(set_selected_to_color), grid, selection_2, Constant(repeat_once(2))),
+        Function(
+            vectorize(set_selected_to_color), grid_node, selection_node_1, Constant(repeat_once(1)),
+        ),
+        Function(
+            vectorize(set_selected_to_color), grid_node, selection_node_2, Constant(repeat_once(1)),
+        ),
+        Function(
+            vectorize(set_selected_to_color), grid_node, selection_node_1, Constant(repeat_once(2)),
+        ),
+        Function(
+            vectorize(set_selected_to_color), grid_node, selection_node_2, Constant(repeat_once(2)),
+        ),
     }
