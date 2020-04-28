5aa56ce8ecfb06f01ef777f23c44ddd00139fdae
Alexander Metz <alexander.wt.metz@gmail.com>
2020-04-28 20:46:42 +0200

    add map color in selection operation

--- solve_arc/language/color.py
+++ solve_arc/language/color.py
@@ -12,11 +12,24 @@ def switch_color(grid, a, b):
 
 def map_color(grid, from_, to):
     mapped = grid.copy()
-    mapped.state[mapped.state == from_] = to
+    mapped.state[grid.state == from_] = to
+    return mapped
+
+
+def map_color_in_selection(grid, selection, from_, to):
+    if grid.shape != selection.shape:
+        return None
+
+    mask = np.logical_and(selection.state, grid.state == from_)
+    mapped = grid.copy()
+    mapped.state[mask] = to
     return mapped
 
 
 def set_selected_to_color(grid, selection, color):
+    if grid.shape != selection.shape:
+        return None
+
     changed = selection.state * color
     unchanged = grid.state * np.invert(selection.state)
     return Grid(changed + unchanged)
--- solve_arc/language/test_color.py
+++ solve_arc/language/test_color.py
@@ -14,11 +14,6 @@ def example_selection():
     return Selection([[True, False, False], [True, True, False], [False, True, True]])
 
 
-def test_map_color(example_grid):
-    expected_grid = Grid([[0, 2, 3], [4, 5, 6], [7, 8, 9]])
-    assert map_color(example_grid, 1, 0) == expected_grid
-
-
 def test_switch_color(example_grid):
     expected_grid = Grid([[2, 1, 3], [4, 5, 6], [7, 8, 9]])
     assert switch_color(example_grid, 1, 2) == expected_grid
@@ -32,3 +27,15 @@ def test_switch_color(example_grid, example_selection):
 def test_switch_color__to_black(example_grid, example_selection):
     expected_grid = Grid([[0, 2, 3], [0, 0, 6], [7, 0, 0]])
     assert set_selected_to_color(example_grid, example_selection, 0) == expected_grid
+
+
+def test_map_color(example_grid):
+    expected_grid = Grid([[0, 2, 3], [4, 5, 6], [7, 8, 9]])
+    assert map_color(example_grid, 1, 0) == expected_grid
+
+
+def test_map_color_in_selection(example_selection):
+    example_grid = Grid([[0, 0, 2], [1, 2, 0], [1, 1, 3]])
+    expected_grid = Grid([[0, 0, 2], [3, 2, 0], [1, 3, 3]])
+
+    assert map_color_in_selection(example_grid, example_selection, 1, 3)
